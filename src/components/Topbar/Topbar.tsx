07b22906fce8fb8a5202720b7978fffef10d0ffa
Valentin Duffet <vavadu07@gmail.com>
2023-07-18 10:39:12 +0200

    sign-out and profile page

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,14 +1,35 @@
 import styles from './Topbar.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaSignOutAlt } from 'react-icons/fa';
 import { FaCircleUser } from 'react-icons/fa6';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  AppDispatch,
+  RootState,
+  dataUserReducer,
+  signInReducer,
+} from '../../store/store';
 
 const Topbar = () => {
   const userLogin = useSelector((state: RootState) => state.login);
   const userData = useSelector((state: RootState) => state.user);
-  console.log(userData);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    dispatch(signInReducer({ statusLogin: false, token: null }));
+    dispatch(
+      dataUserReducer({
+        email: null,
+        firstName: null,
+        lastName: null,
+        createdAt: null,
+        updatedAt: null,
+        id: null,
+      })
+    );
+    navigate('/');
+  };
 
   return (
     <nav className={styles['main-nav']}>
@@ -20,14 +41,20 @@ const Topbar = () => {
         />
         <h1 className='sr-only'>Argent Bank</h1>
       </Link>
-      {userLogin.statusLogin && <span>{userData.firstName}</span>}
-      <div>
+
+      <div className={styles['main-nav-container']}>
+        {userLogin.statusLogin && (
+          <Link className={styles['main-nav-item']} to={'/profile'}>
+            <FaCircleUser />
+            {userData.firstName}
+          </Link>
+        )}
         {userLogin.statusLogin ? (
           // Faire la logique de deconnection navigation
-          <Link className={styles['main-nav-item']} to={'/'}>
+          <div className={styles['sign-out']} onClick={signOut}>
             <FaSignOutAlt />
             Sign Out
-          </Link>
+          </div>
         ) : (
           <Link className={styles['main-nav-item']} to={'/login'}>
             <FaCircleUser />
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,3 +1,4 @@
+import styles from './Profile.module.scss';
 import { useSelector } from 'react-redux';
 import { AppDispatch, RootState, dataUserReducer } from '../../store/store';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +8,7 @@ import { useDispatch } from 'react-redux';
 
 const Profile = () => {
   const userLogin = useSelector((state: RootState) => state.login);
+  const userData = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
@@ -23,7 +25,67 @@ const Profile = () => {
   }, [userLogin.statusLogin]);
 
   if (userLogin.statusLogin) {
-    return <div>Profile</div>;
+    return (
+      <main className={styles['container-profile']}>
+        <div className={styles['header']}>
+          <h1>
+            Welcome back
+            <br />
+            {userData.firstName} {userData.lastName}!
+          </h1>
+          <button className={styles['edit-button']}>Edit Name</button>
+        </div>
+        <h2 className='sr-only'>Accounts</h2>
+        <section className={styles['account']}>
+          <div className={styles['account-content-wrapper']}>
+            <h3 className={styles['account-tittle']}>
+              Argent Bank Checking (x8349)
+            </h3>
+            <p className={styles['account-amount']}>$2,082.79</p>
+            <p className={styles['account-amout-description']}>
+              Available Balance
+            </p>
+          </div>
+          <div className={styles['account-content-wrapper cpa']}>
+            <button className={styles['transaction-button']}>
+              View transactions
+            </button>
+          </div>
+        </section>
+        <section className={styles['account']}>
+          <div className={styles['account-content-wrapper']}>
+            <h3 className={styles['account-tittle']}>
+              Argent Bank Savings (x6712)
+            </h3>
+            <p className={styles['account-amount']}>$10,928.42</p>
+            <p className={styles['account-amout-description']}>
+              Available Balance
+            </p>
+          </div>
+          <div className={styles['account-content-wrapper cpa']}>
+            <button className={styles['transaction-button']}>
+              View transactions
+            </button>
+          </div>
+        </section>
+        <section className={styles['account']}>
+          <div className={styles['account-content-wrapper']}>
+            <h3 className={styles['account-tittle']}>
+              Argent Bank Credit Card (x8349)
+            </h3>
+            <p className={styles['account-amount']}>$184.30</p>
+            <p className={styles['account-amout-description']}>
+              Current Balance
+            </p>
+          </div>
+          <div className={styles['account-content-wrapper cpa']}>
+            <button className={styles['transaction-button']}>
+              View transactions
+            </button>
+          </div>
+        </section>
+      </main>
+    );
   } else {
     return null;
   }
